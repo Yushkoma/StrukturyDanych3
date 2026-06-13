fd65dac46266e9b0dbc1fd773d7d77fb7384ce08
Kornel <kornel.okinczyc@gmail.com>
2026-06-13 19:36:11 +0200

    dodano plik z funckjami do pomiarów

---
+++ include/efficiency_tests.h
@@ -0,0 +1,8 @@
+#include <chrono>
+#include <iostream>
+#include "HT_OpenAddressing.h"
+#include "HT_SeparateChaining.h"
+
+void efficiency_test_OA() {
+    std::cout << "Efficiency test for Open Addressing:" << std::endl;
+}
